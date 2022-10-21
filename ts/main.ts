0c3c30ae1df2919da0312a4521b3b7662d33e6a6
ButlerMW <butler.m.w@outlook.com>
2022-10-20 23:32:18 -0600

    clean up code

--- ts/main.ts
+++ ts/main.ts
@@ -10,42 +10,42 @@ return "Hello World";
 };
 
 function darkModeButton() {
-if(document != null){
+  if(document != null){
 
-  let c = document.getElementById("demo")?.style.color;
-  // console.log(c);
-  if(c === 'white'){
-    
-    document.getElementById("demo")!.style.color = "black";
-    document.getElementById("demo")!.style.backgroundColor = "white";
-    document.getElementById("demo")!.textContent = "Light Mode";
-    
-    document.getElementById("dark-mode-card")!.style.backgroundColor = "rgb(49,49,49)";
+    let c = document.getElementById("demo")?.style.color;
+    // console.log(c);
+    if(c === 'white'){
+      
+      document.getElementById("demo")!.style.color = "black";
+      document.getElementById("demo")!.style.backgroundColor = "white";
+      document.getElementById("demo")!.textContent = "Light Mode";
+      
+      document.getElementById("dark-mode-card")!.style.backgroundColor = "rgb(49,49,49)";
 
-    document.body.style.backgroundColor = "rgb(38,38,38)";
-    
-    var elements = Array.from(document.getElementsByClassName("dark-mode") as HTMLCollectionOf<HTMLElement>)
-    for(var i = 0; i < elements.length; ++i)  {
-      elements[i].style.color = "white";
-    }
-      
-    }else{
-      
-      document.getElementById("demo")!.style.color = "white";
-      document.getElementById("demo")!.style.backgroundColor = "rgb(38,38,38)";
-      document.getElementById("demo")!.textContent = "Dark Mode";
-      
-      document.getElementById("dark-mode-card")!.style.backgroundColor = "lightgrey";
-        
-      document.body.style.backgroundColor = "white";
+      document.body.style.backgroundColor = "rgb(38,38,38)";
       
       var elements = Array.from(document.getElementsByClassName("dark-mode") as HTMLCollectionOf<HTMLElement>)
-
       for(var i = 0; i < elements.length; ++i)  {
-        elements[i].style.color = "black";
+        elements[i].style.color = "white";
       }
-  }
-}    
+        
+      }else{
+        
+        document.getElementById("demo")!.style.color = "white";
+        document.getElementById("demo")!.style.backgroundColor = "rgb(38,38,38)";
+        document.getElementById("demo")!.textContent = "Dark Mode";
+        
+        document.getElementById("dark-mode-card")!.style.backgroundColor = "lightgrey";
+          
+        document.body.style.backgroundColor = "white";
+        
+        var elements = Array.from(document.getElementsByClassName("dark-mode") as HTMLCollectionOf<HTMLElement>)
+
+        for(var i = 0; i < elements.length; ++i)  {
+          elements[i].style.color = "black";
+        }
+    }
+  }    
 }
 
 export {hello, darkModeButton}
